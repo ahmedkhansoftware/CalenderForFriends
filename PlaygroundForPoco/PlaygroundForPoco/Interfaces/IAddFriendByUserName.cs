f229bc44b496bd35f99facea08fc4135ba89514d
Ahmed Khan <ahmedkhansoftware@gmail.com>
2021-10-16 20:57:05 -0700

    Added Interfaces
    Common Behaviors (Interfaces) for this application have been added to
    the PlaygroundForPoco.

---
+++ PlaygroundForPoco/PlaygroundForPoco/Interfaces/IAddFriendByUserName.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlaygroundForPoco.Interfaces
+{
+    // This behavior defines that a user should be able to add friends 
+    // to their friend list by their username.
+    public interface IAddFriendByUserName
+    {
+        // TODO: Functions and Declarations to be added.
+    }
+}
---
+++ PlaygroundForPoco/PlaygroundForPoco/Interfaces/IAddProfilePicture.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlaygroundForPoco.Interfaces
+{
+    // This behavior defines that a user should be able to set their Profile Picture.
+    public interface IAddProfilePicture
+    {
+        // TODO: Functions and Declarations to be added.
+    }
+}
